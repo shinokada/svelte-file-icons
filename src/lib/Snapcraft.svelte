bb95b1139798e9805989055f0b6a31596abd3706
Shinichi Okada <okada.shin@gmail.com>
2023-02-03 09:55:52 +0900

    fix: update index.js

--- src/lib/Snapcraft.svelte
+++ src/lib/Snapcraft.svelte
@@ -1,1 +1,20 @@
-<script>export let size="24"; export let color="currentColor";export let ariaLabel="Snapcraft" </script><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewBox="0 0 512 512" fill={color} class={$$props.class} {...$$restProps} aria-label={ariaLabel} ><path d="M294.5,122l112.9,50.2L294.5,285V122z M79,500.6l199.2-199.2L217.4,241L79,500.6z M0,11.4l284.9,283.2V114.4L0,11.4z M465.1,114.4H301.1L512,208.2L465.1,114.4z"/></svg>
+<script>
+	export let size = '24';
+	export let color = 'currentColor';
+	export let ariaLabel = 'Snapcraft';
+</script>
+
+<svg
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 512 512"
+	fill={color}
+	class={$$props.class}
+	{...$$restProps}
+	aria-label={ariaLabel}
+	><path
+		d="M294.5,122l112.9,50.2L294.5,285V122z M79,500.6l199.2-199.2L217.4,241L79,500.6z M0,11.4l284.9,283.2V114.4L0,11.4z M465.1,114.4H301.1L512,208.2L465.1,114.4z"
+	/></svg
+>
